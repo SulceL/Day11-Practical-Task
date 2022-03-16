e46447c1f52cb82a513048c8094fdd112100c83e
Linda Šulce <linda.sulce@gmail.com>
2022-03-16 20:45:42 +0200

    Irena, Julija, Violeta (Linda typing because of internet issues)

--- Day11-Practical-Task/Program.cs
+++ Day11-Practical-Task/Program.cs
@@ -22,4 +22,35 @@ for (int i = 0; i < myArray.Length; i++)
         Console.Write($" {myArray[i]} ");
     }
    
-} 
+}
+Console.WriteLine();
+Console.WriteLine();
+// Task 2 
+
+string []myStringArray = new string [4];
+myStringArray[0] = "Linda";
+myStringArray[1] = "Irena";
+myStringArray[2] = "Julija";
+myStringArray[3] = "Violeta";
+
+for (int i = 0; i < myStringArray.Length; i++)
+{
+    Console.WriteLine($" {myStringArray[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine();
+//Task3
+string[,] myStringArray2D = new string[2,2];
+myStringArray2D[0,0] = "Linda";
+myStringArray2D[0,1] = "Irena";
+myStringArray2D[1,0] = "Julija";
+myStringArray2D[1,1] = "Violeta";
+
+
+for (int i = 0; i < myStringArray2D.GetLength(0); i++)
+{
+    for (int k = 0; k < myStringArray2D.GetLength(1); k++)
+    { 
+     Console.WriteLine ($"{myStringArray2D [i,k]} "); 
+    }
+}
