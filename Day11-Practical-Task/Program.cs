4f0bbf296d48ed8b95fa476d428e89abf57dd231
Linda Šulce <linda.sulce@gmail.com>
2022-03-16 19:48:54 +0200

    Task 1 done. LindaS

--- Day11-Practical-Task/Program.cs
+++ Day11-Practical-Task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Day 11 Task 1
+
+int[]myArray =new int[5];
+var random = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = random.Next();
+}
+Console.WriteLine($"This array consists of numbers");
+
+for (int i = 0; i < myArray.Length; i++)
+{ 
+    Console.Write($" {myArray[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine("Odd numbers of the array are:");
+for (int i = 0; i < myArray.Length; i++)
+{
+    var oddCondition = myArray[i] % 2 != 0;
+    if (oddCondition)
+    {
+        Console.Write($" {myArray[i]} ");
+    }
+   
+} 
